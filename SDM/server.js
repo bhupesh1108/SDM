c67d3efeba02f3ca1b3ee23991dfdc6d4b3e51cd
bhupesh1108 <bhupesh.verma00700@gmail.com>
2024-01-06 00:42:24 +0530

    'new'

--- SDM/server.js
+++ SDM/server.js
@@ -6,7 +6,7 @@ const port=8000;
 const publicDirectory=path.join(__dirname,'/');
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(publicDirectory,'/index.html'));
+    res.sendFile(path.join(publicDirectory,'public/index.html'));
 });
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`);
